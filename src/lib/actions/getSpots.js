0b95213011701632b09ea44600e14c8d8d3c19e3
aurelienLRY <leroyaurelien11@gmail.com>
2024-06-24 14:23:05 +0200

    suite

--- src/lib/actions/getSpots.js
+++ src/lib/actions/getSpots.js
@@ -5,7 +5,6 @@ import Spots from "@/lib/models/spots";
 export default async function getSpots() {
   try {
     const spots = await Spots.find().lean();
-    console.log("getSpots>>>>",spots);
     return spots;
   } catch (error) {   
     disconnectDB();
